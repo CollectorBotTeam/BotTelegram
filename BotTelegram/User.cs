3e27896539e934f11dcde8213de5f932e36701ca
Антон Коновалов < >
2016-03-12 00:03:10 +0300

    Init Anton

---
+++ BotTelegram/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BotTelegram
+{
+    public class User : Person
+    {
+        public FriendList FriendList
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public MyLoansRegister MyLoansRegister
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public DebtorList DebtorList
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
